7abf223172d04546aa482e5d23391dfff6cfadc6
luvumbu <luvumbu.n@gmail.com>
2024-11-14 01:28:10 +0100

    ok

--- index.php
+++ index.php
@@ -5,30 +5,25 @@ session_start();
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 
 <body>
-<?php 
-
-
-
+  <?php
   // Inclusion des fichiers de classe PHP nécessaires
   require_once 'Class/path_general_class.php';
- 
-// Example usage
-$path = "Class/dbCheck.php";
-if (checkFileExists($path)) {
-  require_once  'Class/dbCheck.php' ; 
-   require_once 'view/form_creation_table_bdd.php' ;  
-}
-else {
-    require_once 'view/loginVerificationForm.php' ; 
-  } 
+  // Example usage
+  $path = "Class/dbCheck.php";
+  if (checkFileExists($path)) {
+    require_once  'Class/dbCheck.php';
+    require_once 'view/form_creation_table_bdd.php';
+  } else {
+    require_once 'view/loginVerificationForm.php';
+  }
   ?>
-<script src="Class/js.js"></script> 
- <?php 
-require_once 'log.php' ; 
-?>
+  <script src="Class/js.js"></script>
+  <?php
+  require_once 'log.php';
+  ?>
