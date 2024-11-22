009247dc6ad10b66d4be6792938c86d129dc2355
luvumbu <luvumbu.n@gmail.com>
2024-11-22 19:27:42 +0100

    add nouvelle methode  ok

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="icon" type="image/x-icon" href="https://ih1.redbubble.net/image.1762483057.7729/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.webp">
 </head>
 <style>
   .display_none {
@@ -30,40 +31,34 @@ session_start();
       if (file_exists($path)) {
         // Tente de supprimer le fichier
         if (unlink($path)) {
-            echo "Le fichier '$path' a été supprimé avec succès.";
-          ?>
-  <meta http-equiv="refresh" content="0"> <!-- Rafraîchit toutes les 5 secondes -->
-          <?php 
+          echo "Le fichier '$path' a été supprimé avec succès.";
+  ?>
+          <meta http-equiv="refresh" content="0"> <!-- Rafraîchit toutes les 5 secondes -->
+        <?php
         } else {
-            echo "Erreur : Impossible de supprimer le fichier '$path'.";
+          echo "Erreur : Impossible de supprimer le fichier '$path'.";
         }
-    } else {
+      } else {
         echo "Le fichier '$path' n'existe pas.";
-    }
-
-
-    } else {     
+      }
+    } else {
       // require_once 'view/form_creation_table_bdd.php';
       //  require_once 'view/test.php';
       if (isset($_SESSION["index"])) {
-        require_once 'view/home.php' ;
-   
-?>
-
-<a href="req/session_destroy.php">
-    <img width="100" height="100" src="https://img.icons8.com/fluency/100/switch-off.png" alt="switch-off"/>
-
-    </a>
-<?php 
+        require_once 'view/home.php';
+        ?>
+        <a href="req/session_destroy.php">
+          <img width="100" height="100" src="https://img.icons8.com/fluency/100/switch-off.png" alt="switch-off" />
+        </a>
+  <?php
         if ($_SESSION["index"][0] == $dbname && $_SESSION["index"][1]  == $username) {
+
+          echo '<div class="display_none">';
           require_once 'view/admin_form_creation_table_bdd.php';
+          echo '</div>';
         } else {
-
-
- 
         }
       } else {
-
         require_once 'view/verifyConnection.php';
       }
     }
@@ -75,20 +70,10 @@ session_start();
   <?php
   // require_once 'log/log.php';
   // requare all 
-
-
   ?>
   <div id="dowload_file" class="display_none">
     <?php
     require_once 'view/dowload_file.php';
     // cette log permet de charger toute les fuction ont peut les deselectionner pour choissir cel qui conviens 
-
-
-
     ?>
   </div>
-
-
- 
-
-
--- view/home.php
+++ view/home.php
@@ -1,8 +1,62 @@
-<?php 
-
-require_once 'function/select/general_root_.php' ;  
-var_dump($id_user) ; 
-?>
-
-
 <h1>HELLO </h1>
+
+
+<div class="parent_projet">
+    <div class="parent_projet0">
+        <div class="parent_projet1">
+            <img src="https://cdn.pixabay.com/photo/2019/10/16/09/09/doraemon-4553920_1280.png" alt="" srcset="">
+        </div>
+        <div class="parent_projet2">
+            <h2>Card</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aspernatur aut dolor eaque provident facere inventore
+                commodi fugiat earum illo necessitatibus voluptatibus
+                ab, reiciendis
+                tempore eius, dolore corrupti, vel ipsum. Doloremque.
+            </p>
+        </div>
+    </div>
+
+</div>
+<style>
+ 
+
+    .parent_projet1,
+    .parent_projet2 {
+
+        width: 300px;
+        margin: auto;
+
+    }
+
+    .parent_projet2 {
+        text-align: center;
+    }
+
+    .parent_projet2 p {
+        text-align: justify;
+    }
+
+    .parent_projet1 img {
+        width: 100%;
+    }
+    .parent_projet1 img:hover {
+     cursor: pointer;
+    }
+
+    .parent_projet0 {
+        border: 1px solid rgba(0, 0, 0, 0.3);
+        width: 300px;
+    
+        margin: auto;
+    }
+
+    .parent_projet0 {
+        border: 1px solid rgba(0, 0, 0, 0.3);
+        width: 300px;
+        padding: 15px;
+    
+        margin: auto;
+    }
+</style>
