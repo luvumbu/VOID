ee62ad18698698228f727317353fb29694c249ea
luvumbu <luvumbu.n@gmail.com>
2024-11-24 23:42:12 +0100

    toutes les operation sont ok

--- view/home.php
+++ view/home.php
@@ -1,11 +1,4 @@
  <?php
 require_once "home_nom_user.php";
-
-
-
-
 ?> 
-
-<div onclick="general_js(this)" title="function/general_remove.php"  class="projet id_projet 2"    style="background-color: black;padding:15px;color:white">
-REMOVE
-</div>
+ 
