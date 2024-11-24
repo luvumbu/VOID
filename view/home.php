c463d41083e1bba35ea61dc495175bb188581e17
luvumbu <luvumbu.n@gmail.com>
2024-11-24 22:45:21 +0100

    option remove okokokk

--- view/home.php
+++ view/home.php
@@ -6,6 +6,6 @@ require_once "home_nom_user.php";
 
 ?> 
 
-<div onclick="general_js(this)" title="function/general_remove.php"  class="projet id_projet 1"    style="background-color: black;padding:15px;color:white">
+<div onclick="general_js(this)" title="function/general_remove.php"  class="projet id_projet 2"    style="background-color: black;padding:15px;color:white">
 REMOVE
 </div>
