a72a41e6abe93a87b892be75136e420110c1c854
luvumbu <luvumbu.n@gmail.com>
2024-12-06 07:05:33 +0100

    generateur php 1/3 complet

--- config/auto_exe.php
+++ config/auto_exe.php
@@ -3,277 +3,127 @@ $path_general1 = "../function/";
 $path_general_json = "../function_json/";
 $src_general1 = "../Class/";
 $src_general2 = "Class/";
-$path_bool  ; 
+$path_bool;
 
 $databaseHandler = new DatabaseHandler($dbname, $username);
-// Récupérer la liste des tables
 $databaseHandler->getTables($nomBaseDeDonnees);
 $databaseHandler->getListOfTables_Child($mainTableName);
 $mainTableName = $tables[$a];
 $add_file_general0 = "<?php \n";
-$add_file_general0 .=
-    <<<'PHP'
+$add_file_general0 .= <<<'PHP'
 require_once "general_start.php";
-
-
-if(isset($_POST["option0_1"])){
-    $option0_1 =$_POST["option0_1"] ;
-}
-if(isset($_POST["option0_2"])){
-    $option0_2 =$_POST["option0_2"] ;
-}        
-if(isset($_POST["option1_1"])){
-    $option1_1 =$_POST["option1_1"] ;
-}
-if(isset($_POST["option1_2"])){
-    $option1_2 =$_POST["option1_2"] ;
-}
-if(isset($_POST["option2_1"])){
-    $option2_1 =$_POST["option2_1"] ;
-}
-if(isset($_POST["option2_2"])){
-    $option2_2 =$_POST["option2_2"] ;
-}
-if(isset($_POST["option3_1"])){
-    $option3_1 =$_POST["option3_1"] ;
-}
-if(isset($_POST["option3_2"])){
-    $option3_2 =$_POST["option3_2"] ;
-}
-
-
+if(isset($_POST["option0_1"])){ $option0_1 =$_POST["option0_1"]; }
+if(isset($_POST["option0_2"])){ $option0_2 =$_POST["option0_2"]; }
+if(isset($_POST["option1_1"])){ $option1_1 =$_POST["option1_1"]; }
+if(isset($_POST["option1_2"])){ $option1_2 =$_POST["option1_2"]; }
+if(isset($_POST["option2_1"])){ $option2_1 =$_POST["option2_1"]; }
+if(isset($_POST["option2_2"])){ $option2_2 =$_POST["option2_2"]; }
+if(isset($_POST["option3_1"])){ $option3_1 =$_POST["option3_1"]; }
+if(isset($_POST["option3_2"])){ $option3_2 =$_POST["option3_2"]; }
 $servername = "localhost";
 $option0_2 = $_POST["option0_2"];
 if(isset($_POST["option00"])){
- $src_general1 = "../Class/";
- $path_bool  =0; 
+$src_general1 = "../Class/";
+$path_bool = 0;
 }
 else {
 $src_general1 = "Class/";
- $path_bool  =1; 
-
+$path_bool = 1;
 }
- 
 require_once $src_general1."dbCheck.php";
 require_once $src_general1."Give_url.php";
 require_once $src_general1."DatabaseHandler.php";
 require_once $src_general1."AsciiConverter.php";
 $databaseHandler = new DatabaseHandler($dbname, $username);
-
 PHP;
 
-
-
-
 $add_file_general1 = $add_file_general0;
-$add_file_general1 .=     <<<'PHP'
-
- 
+$add_file_general1 .= <<<'PHP'
 $url = new Give_url();
 $url->split_basename('__');
-
-$count_url = count($url->get_elements()) ; 
- 
-
-
-/*
-// model exemple d'utilisation coté utilisateur
-<script>
-    function afficherValeursFormattees(chaine) {
-        // Diviser la chaîne par le caractère "_"
-        const valeurs = chaine.split("__");
-        // Retourner les valeurs dans un tableau
-        return valeurs;
-    }
-
-
-
-
-
-    function general_send(_this) {
-
- 
-
-   // <div id="" onclick="general_send(this)" class="add_1__projet__id_sha1_user__<?php echo $_SESSION["index"][2] ?>">
-   //     ADD
-   //  </div>
-
-    
-
-
-        // Exemple d'utilisation
-
-        const texteFormatte = afficherValeursFormattees(_this.className);
-        console.log(texteFormatte);
-
-
-
-        var ok = new Information("function/general.php"); // création de la classe 
-
-
-        ok.add("option00", ""); // ajout de l'information pour lenvoi 
-        if (texteFormatte[0] != undefined) {
-            ok.add("option0_1", texteFormatte[0]); // ajout de l'information pour lenvoi 
-        }
-        if (texteFormatte[1] != undefined) {
-            ok.add("option0_2", texteFormatte[1]); // ajout de l'information pour lenvoi
-        }
-        if (texteFormatte[2] != undefined) {
-            ok.add("option1_1", texteFormatte[2]); // ajout de l'information pour lenvoi 
-        }
-        if (texteFormatte[3] != undefined) {
-            ok.add("option1_2", texteFormatte[3]); // ajout de l'information pour lenvoi 
-        }
-        if (texteFormatte[4] != undefined) {
-            ok.add("option2_1", texteFormatte[4]); // ajout de l'information pour lenvoi 
-        }
-        if (texteFormatte[5] != undefined) {
-            ok.add("option2_2", texteFormatte[5]); // ajout de l'information pour lenvoi 
-        }
-        if (texteFormatte[6] != undefined) {
-            ok.add("option3_1", texteFormatte[6]); // ajout de l'information pour lenvoi 
-        }
-        if (texteFormatte[7] != undefined) {
-            ok.add("option3_2", texteFormatte[7]); // ajout de l'information pour lenvoi 
-        }
-        console.log(ok.info()); // demande l'information dans le tableau
-        ok.push(); // envoie l'information au code pkp 
-
-    }
-</script>
-
-
-*/
-
-
-
- switch ($option0_1) {
-    case 'add_1':
-
-
-
-    $databaseHandler->action_sql("INSERT INTO `$option0_2` ($option1_1) VALUES ('$option1_2')") ;
-
-// exemple d'utilisation 
-
+$count_url = count($url->get_elements());
 /*
 <script>
-    
-var ok = new Information("function/general.php"); // création de la classe 
-ok.add("option00", "root"); // ajout de l'information pour lenvoi 
-ok.add("option0_1", "root"); // ajout de l'information pour lenvoi 
-ok.add("option0_2", "add_1"); // ajout de l'information pour lenvoi 
-
-ok.add("option1_1", "id_sha1_user"); // ajout de l'information pour lenvoi 
-ok.add("option1_2", "xXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"); // ajout de l'information pour lenvoi 
- 
- 
-ok.push(); // envoie l'information au code pkp 
-
-
-</script>
-
-*/
-
-        break;
-
-    case 'add_2':
-
-       $databaseHandler->action_sql("INSERT INTO `$option0_2` ($option1_1,$option2_1) VALUES ('$option1_2','$option2_2')") ;
-
-
-
-
-
-        break;
-
-    case 'add_3':
-
-        $databaseHandler->action_sql("INSERT INTO `$option0_2` ($option1_1,$option2_1,$option3_1) VALUES ('$option1_2','$option2_2','$option3_2')") ;
-
-        break;
-
-    case 'remove_1':
-
-        $databaseHandler->action_sql("DELETE FROM  `$option0_2` WHERE   `$option1_1` = '$option1_2'") ;
- 
-        break;        
-
-    case 'update_1':
- 
-     $databaseHandler->action_sql("UPDATE  `$option0_2` SET `$option1_1` = '$option1_2'   WHERE  `$option2_1` ='$option2_2' ");
- 
-       
-       break;
-
-       /*
-    case 'update_2':
-
-        $databaseHandler->action_sql("UPDATE  `$option0_2` SET `$option2_1` = '$option2_2', `$option3_1` = '$option3_2'   WHERE  `$option1_1` ='$option1_2' ");
-
-        break; 
-    case 'update_3':
-
-        $databaseHandler->action_sql("UPDATE  `$option0_2` SET `$option2_1` = '$option2_2', `$option3_1` = '$option3_2', `$option4_1` = '$option4_2'   WHERE  `$option1_1` ='$option1_2' ");
-
-        break; 
-        */
-
+function afficherValeursFormattees(chaine) {
+const valeurs = chaine.split("__");
+return valeurs;
 }
-
- 
-
- switch ($count_url) {
-
-    case '3':
-        $url_info = $url->get_elements();
-        $option0_1 = $url_info[0];
-        $option1_1 = $url_info[1];
-        $option1_2 = $url_info[2];
-        // Exemple d'utilisation n°6
-        // Nom de la table 
-        // puis de la colllone qu'on voudrais afficher
-        $nom_table = $dbname;
-        // nom de la table
-        $req_sql = "SELECT * FROM `$nom_table` WHERE $option1_1 = $option1_2";
-        $databaseHandler->getListOfTables_Child($nom_table);
-        $databaseHandler->getDataFromTable2X($req_sql);
-        $databaseHandler->get_dynamicVariables();
-       // var_dump($dynamicVariables['id_sha1_user']);
-        // cet exemple permet de voir la liste total des element efant 
-        // avec la valeur detaille sans perte de performance  
-
-        break;
-
-    case '5':
-
-        $url_info = $url->get_elements();
- 
-
-        $option0_1 =$url_info[0];
-        $option1_1 =$url_info[1] ; 
-        $option1_2 =$url_info[2] ;
-        
-        $option2_1 =$url_info[3];
-        $option2_2 =$url_info[4] ; 
- 
-
-        // Exemple d'utilisation n°6
-        // Nom de la table 
-        // puis de la colllone qu'on voudrais afficher
-        $nom_table = $dbname;
-        // nom de la table
-        $req_sql = "SELECT * FROM `$nom_table` WHERE $option1_1 = '$option1_2' AND $option2_1 = '$option2_2'" ;
-        $databaseHandler->getListOfTables_Child($nom_table);
-        $databaseHandler->getDataFromTable2X($req_sql);
-        $databaseHandler->get_dynamicVariables();
-
-        //   var_dump($url->get_elements()) ; 
-       // var_dump($dynamicVariables['id_sha1_user']);
-        // cet exemple permet de voir la liste total des element efant 
-        // avec la valeur detaille sans perte de performance  
-        break;
-
+function general_send(_this) {
+const texteFormatte = afficherValeursFormattees(_this.className);
+console.log(texteFormatte);
+var ok = new Information("function/general.php");
+ok.add("option00", "");
+if (texteFormatte[0] != undefined) {
+ok.add("option0_1", texteFormatte[0]);
+}
+if (texteFormatte[1] != undefined) {
+ok.add("option0_2", texteFormatte[1]);
+}
+if (texteFormatte[2] != undefined) {
+ok.add("option1_1", texteFormatte[2]);
+}
+if (texteFormatte[3] != undefined) {
+ok.add("option1_2", texteFormatte[3]);
+}
+if (texteFormatte[4] != undefined) {
+ok.add("option2_1", texteFormatte[4]);
+}
+if (texteFormatte[5] != undefined) {
+ok.add("option2_2", texteFormatte[5]);
+}
+if (texteFormatte[6] != undefined) {
+ok.add("option3_1", texteFormatte[6]);
+}
+if (texteFormatte[7] != undefined) {
+ok.add("option3_2", texteFormatte[7]);
+}
+console.log(ok.info());
+ok.push();
+}
+</script>
+*/
+switch ($option0_1) {
+case 'add_1':
+$databaseHandler->action_sql("INSERT INTO `$option0_2` ($option1_1) VALUES ('$option1_2')");
+break;
+case 'add_2':
+$databaseHandler->action_sql("INSERT INTO `$option0_2` ($option1_1,$option2_1) VALUES ('$option1_2','$option2_2')");
+break;
+case 'add_3':
+$databaseHandler->action_sql("INSERT INTO `$option0_2` ($option1_1,$option2_1,$option3_1) VALUES ('$option1_2','$option2_2','$option3_2')");
+break;
+case 'remove_1':
+$databaseHandler->action_sql("DELETE FROM  `$option0_2` WHERE   `$option1_1` = '$option1_2'");
+break;
+case 'update_1':
+$databaseHandler->action_sql("UPDATE  `$option0_2` SET `$option1_1` = '$option1_2'   WHERE  `$option2_1` ='$option2_2'");
+break;
+}
+switch ($count_url) {
+case '3':
+$url_info = $url->get_elements();
+$option0_1 = $url_info[0];
+$option1_1 = $url_info[1];
+$option1_2 = $url_info[2];
+$nom_table = $dbname;
+$req_sql = "SELECT * FROM `$nom_table` WHERE $option1_1 = $option1_2";
+$databaseHandler->getListOfTables_Child($nom_table);
+$databaseHandler->getDataFromTable2X($req_sql);
+$databaseHandler->get_dynamicVariables();
+break;
+case '5':
+$url_info = $url->get_elements();
+$option0_1 = $url_info[0];
+$option1_1 = $url_info[1];
+$option1_2 = $url_info[2];
+$option2_1 = $url_info[3];
+$option2_2 = $url_info[4];
+$nom_table = $dbname;
+$req_sql = "SELECT * FROM `$nom_table` WHERE $option1_1 = '$option1_2' AND $option2_1 = '$option2_2'";
+$databaseHandler->getListOfTables_Child($nom_table);
+$databaseHandler->getDataFromTable2X($req_sql);
+$databaseHandler->get_dynamicVariables();
+break;
 }
 ?>
 PHP;
@@ -282,160 +132,45 @@ $filePath =  $path_general1 . "/general.php";
 $source_file_array = [];
 $fileHandler = new FileHandler($filePath, $add_file_general1, $source_file_array);
 $fileHandler->processFile();
-// Afficher les fichiers ajoutés
-//print_r($source_file_array);
-
-
-
-
- 
-
-
-
-
- 
-$add_file_general2 =     <<<'PHP'
+$add_file_general2 = <<<'PHP'
 <?php 
 require_once "general_start.php";
 $servername = "localhost";
-
-$src_general1 = "Class/" ; 
+$src_general1 = "Class/" ;
 require_once $src_general1."dbCheck.php";
 require_once $src_general1."Give_url.php";
 require_once $src_general1."DatabaseHandler.php";
 require_once $src_general1."AsciiConverter.php";
 $databaseHandler = new DatabaseHandler($dbname, $username);
-
-$option0_1 =$_SESSION["option0_1"] ;
-$option0_2 =$_SESSION["option0_2"] ;
-$option1_1 =$_SESSION["option1_1"] ;
-$option1_2 =$_SESSION["option1_2"] ; 
-$option2_1 =$_SESSION["option2_1"] ;
-$option2_2 =$_SESSION["option2_2"] ; 
-$option3_1 =$_SESSION["option3_1"] ;
-$option3_2 =$_SESSION["option3_2"] ; 
-
+$option0_1 =$_SESSION["option0_1"];
+$option0_2 =$_SESSION["option0_2"];
+$option1_1 =$_SESSION["option1_1"];
+$option1_2 =$_SESSION["option1_2"];
+$option2_1 =$_SESSION["option2_1"];
+$option2_2 =$_SESSION["option2_2"];
+$option3_1 =$_SESSION["option3_1"];
+$option3_2 =$_SESSION["option3_2"];
 switch ($option0_1) {
-  case "1":
-
-// Exemple d'utilisation n°6
-// Nom de la table 
-// puis de la colllone qu'on voudrais afficher
+case "1":
 $nom_table = $dbname;
-// nom de la table
-$req_sql = "SELECT * FROM `$option0_2` WHERE $option1_1 = '$option1_2'" ;
+$req_sql = "SELECT * FROM `$option0_2` WHERE $option1_1 = '$option1_2'";
 $databaseHandler->getListOfTables_Child($option0_2);
 $databaseHandler->getDataFromTable2X($req_sql);
 $databaseHandler->get_dynamicVariables();
-
-    
-    break;
-  case "2":
-
-// Exemple d'utilisation n°6
-// Nom de la table 
-// puis de la colllone qu'on voudrais afficher
+break;
+case "2":
 $nom_table = $dbname;
-// nom de la table
-$req_sql = "SELECT * FROM `$nom_table` WHERE $option1_1 = '$option1_2' AND $option2_1 = '$option2_2'" ;
+$req_sql = "SELECT * FROM `$nom_table` WHERE $option1_1 = '$option1_2' AND $option2_1 = '$option2_2'";
 $databaseHandler->getListOfTables_Child($nom_table);
 $databaseHandler->getDataFromTable2X($req_sql);
 $databaseHandler->get_dynamicVariables();
-
- // var_dump($url->get_elements()) ; 
-// var_dump($dynamicVariables['id_sha1_user']);
-// cet exemple permet de voir la liste total des element efant 
-// avec la valeur detaille sans perte de performance  
-/*
- var_dump($dynamicVariables['id_sha1_user']);
-<script>
-    
-        var ok = new Information("function/general.php"); // création de la classe 
-        ok.add("option00", "root"); // ajout de l'information pour lenvoi 
-        ok.add("option0_1", "root"); // ajout de l'information pour lenvoi 
-        ok.add("option0_2", "add_3"); // ajout de l'information pour lenvoi 
-
-        ok.add("option1_1", "id_sha1_user"); // ajout de l'information pour lenvoi 
-        ok.add("option1_2", "xXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"); // ajout de l'information pour lenvoi 
-        
-        ok.add("option2_1", "id_parent_user"); // ajout de l'information pour lenvoi 
-        ok.add("option2_2", "test ok"); // ajout de l'information pour lenvoi
-
-        ok.add("option3_1", "description_user"); // ajout de l'information pour lenvoi 
-        ok.add("option3_2", "test ok description_user"); // ajout de l'information pour lenvoi
-
-        console.log(ok.info()); // demande l'information dans le tableau
-        ok.push(); // envoie l'information au code pkp 
-
-
-</script>
-
-*/
-
-    break; 
+break;
 }
-
-
 ?>
 PHP;
 
-
- 
-
-
-$filePath =  $path_general1 . "/session.php";
+$filePath =  $path_general1 . "/general_end.php";
 $source_file_array = [];
 $fileHandler = new FileHandler($filePath, $add_file_general2, $source_file_array);
 $fileHandler->processFile();
-// Afficher les fichiers ajoutés
-//print_r($source_file_array);
-
-
-
- 
-
-
-$general_file =     <<<'PHP'
-<?php  
-
-
 ?>
-PHP;
-
-
-
-$filePath =  $path_general1 . "/general_start.php";
-$source_file_array = [];
-$fileHandler = new FileHandler($filePath, $general_file , $source_file_array);
-$fileHandler->processFile();
-// Afficher les fichiers ajoutés
-//print_r($source_file_array);
-
-
-
-
-
-
-$general_file =     <<<'PHP'
-<?php  
-
-
-?>
-PHP;
-
-
-
-$filePath =  $path_general1 . "/general_start.php";
-$source_file_array = [];
-$fileHandler = new FileHandler($filePath, $general_file , $source_file_array);
-$fileHandler->processFile();
-// Afficher les fichiers ajoutés
-//print_r($source_file_array);
-
-
-
-
- 
-
-
-
--- view/home.php
+++ view/home.php
@@ -1,106 +1,137 @@
- 
-    <!-- Section avec un événement 'onclick' pour déclencher la fonction 'update1' -->
-    <div id="update_1__update_1_text01" onclick="update1(this)" class="update_1__root__id_sha1_user__NEUTRE__id_user__43">
-        ADD
-    </div>
- 
+<?php
+session_start();
 
+class DivGenerator
+{
+    public $name;
+    public $function_name;
+    public $className_array;
+    public $className_array_total;
+    public $function_split ;
+    public $dbname ; 
 
-<textarea name="" id="update_1_text01">
- Lorem ipsum dolor sit ame't consect''etur adipi"sicing""" elit. Illo sint, aliquam ut animi quasi ad asperiores distinctio debitis, recusandae nisi dolorum, excepturi molestiae eligendi quisquam corrupti in itaque necessitatibus sit.
-
-</textarea>
-
- 
-
-<script>
-    // Fonction pour diviser une chaîne par "__" et retourner un tableau de valeurs
-    function afficherValeursFormattees(chaine) {
-        const valeurs = chaine.split("__"); // Séparation de la chaîne par "__"
-        return valeurs; // Retourne le tableau de valeurs
+    function __construct($dbname,$name, $function_name,$function_split)
+    {
+        $this->dbname = $dbname;
+        $this->name = $name;
+        $this->function_name = $function_name;
+        $this->function_split = $function_split;        
     }
 
-    // Fonction principale appelée au clic de l'élément HTML
-    function update1(_this) {
-        // Exemple de la structure de l'élément sur lequel on a cliqué
-        // <div id="" onclick="update1(this)" class="add_1__root__id_sha1_user__Valeur_a_ajouter__43">
-        //      ADD
-        //  </div>
-
-        // Récupération et formatage des valeurs de la classe de l'élément cliqué
-        const texteFormatte = afficherValeursFormattees(_this.className);
-        console.log(texteFormatte); // Affichage des valeurs formatées dans la console
+    function set_className($nom_class, $parametre)
+    {
 
 
+        $nom_class = htmlspecialchars($this->function_split.$nom_class.$this->function_split);
+        $parametre = htmlspecialchars($parametre);
+        $this->className_array[] = [$nom_class, $parametre]; // Ajoute une nouvelle ligne avec 2 éléments
+    }
 
-        const texteFormatte2 = afficherValeursFormattees(_this.id);
+    function className_array()
+    {
 
+        $name=$this->function_name.$this->function_split.$this->dbname ; 
+        $count = count($this->className_array);
 
-
-        // Création d'une instance de la classe 'Information' pour envoyer les données
-        var ok = new Information("function/general.php");
-
-        // Ajout de la première information (option00 est vide ici)
-        ok.add("option00", "");
-
-        // Si la première valeur existe, on l'ajoute à l'instance 'ok'
-
-        // Répétition du processus pour chaque valeur possible (jusqu'à 8 valeurs)
-        if (texteFormatte[1] != undefined) {
-            ok.add("option0_2", texteFormatte[1]);
-        }
-        if (texteFormatte[2] != undefined) {
-            ok.add("option1_1", texteFormatte[2]);
-        }
-        if (texteFormatte[3] != undefined) {
-            ok.add("option1_2", texteFormatte[3]);
-        }
-        if (texteFormatte[4] != undefined) {
-            ok.add("option2_1", texteFormatte[4]);
-        }
-        if (texteFormatte[5] != undefined) {
-            ok.add("option2_2", texteFormatte[5]);
-        }
-        if (texteFormatte[6] != undefined) {
-            ok.add("option3_1", texteFormatte[6]);
-        }
-        if (texteFormatte[7] != undefined) {
-            ok.add("option3_2", texteFormatte[7]);
-        }
-        if (texteFormatte[0] != undefined) {
-            ok.add("option0_1", texteFormatte[0]);
-            if (texteFormatte2[0] == texteFormatte[0]) {
-
-
-                var value = document.getElementById(texteFormatte2[1]).value;
-
-                value.replace("'", "&#39");
-                value.replace('"', "&quot");
-                var new_val = "";
-                for (var boucle = 0; boucle < value.length; boucle++) {
-                    
-
-                    switch (value[boucle]) {
-                        case "'":
-                           new_val = new_val+"&apos" ; 
-                            break;
-                        case '"':
-                           new_val = new_val+"&quot" ; 
-                            break;
-                        default:
-                        new_val = new_val+value[boucle] ;                         
-                    }
-
-                }
-         
-                ok.add("option1_2",new_val );
-
+        for ($a = 0; $a < $count; $a++) {
+            for ($b = 0; $b < $count; $b++) {
+                $name = $name .$this->className_array[$a][$b] ;
             }
         }
-
-
-        console.log(ok.info()); // Affiche les informations collectées dans la console
-        ok.push(); // Envoie les informations collectées vers le script PHP via la méthode 'push'
+        $this->className_array_total = $name ;     
     }
-</script>
+
+    // Méthode pour générer le div
+    public function generateDiv()
+    {
+        $divHtml = '<div id="' . $this->name . '" onclick="' . $this->function_name . '(this)" class="' .$this->className_array_total . '">ADD</div>';
+
+        return $divHtml; // Retourne le code HTML généré
+    }
+}
+
+// Instanciation de la classe DivGenerator
+$divGenerator = new DivGenerator("root","div_generator", "add_2","__");
+$divGenerator->set_className("id_sha1_user", "Valeur a ajouter");
+$divGenerator->set_className("id_parent_user", "ok bebe ");
  
+
+
+
+
+$divGenerator->className_array();
+
+
+
+// Appel direct de la méthode pour générer le div
+ echo  $divGenerator->generateDiv(); // Cela va directement afficher le div généré
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page avec Div Dynamique</title>
+    <script>
+        // Fonction pour diviser une chaîne par "__" et retourner un tableau de valeurs
+        function afficherValeursFormattees(chaine) {
+            const valeurs = chaine.split("__");
+            return valeurs; // Retourne le tableau des valeurs obtenues
+        }
+
+        // Fonction principale appelée au clic de l'élément HTML
+        function add_2(_this) {
+            // Récupération et formatage des valeurs de la classe de l'élément cliqué
+            const texteFormatte = afficherValeursFormattees(_this.className);
+            console.log(texteFormatte); // Affichage des valeurs formatées dans la console
+
+            // Création d'une instance de la classe 'Information' pour envoyer les données
+            var ok = new Information("function/general.php");
+
+            // Ajout de la première information (option00 est vide ici)
+            ok.add("option00", "");
+
+            // Si la première valeur existe, on l'ajoute à l'instance 'ok'
+            if (texteFormatte[0] != undefined) {
+                ok.add("option0_1", texteFormatte[0]);
+            }
+            // Répétition du processus pour chaque valeur possible (jusqu'à 8 valeurs)
+            if (texteFormatte[1] != undefined) {
+                ok.add("option0_2", texteFormatte[1]);
+            }
+            if (texteFormatte[2] != undefined) {
+                ok.add("option1_1", texteFormatte[2]);
+            }
+            if (texteFormatte[3] != undefined) {
+                ok.add("option1_2", texteFormatte[3]);
+            }
+            if (texteFormatte[4] != undefined) {
+                ok.add("option2_1", texteFormatte[4]);
+            }
+            if (texteFormatte[5] != undefined) {
+                ok.add("option2_2", texteFormatte[5]);
+            }
+            if (texteFormatte[6] != undefined) {
+                ok.add("option3_1", texteFormatte[6]);
+            }
+            if (texteFormatte[7] != undefined) {
+                ok.add("option3_2", texteFormatte[7]);
+            }
+
+            console.log(ok.info()); // Affiche les informations collectées dans la console
+            ok.push(); // Envoie les informations collectées vers le script PHP via la méthode 'push'
+        }
+    </script>
+</head>
+
+<body>
+
+    <!-- Affichage du div généré par PHP -->
+    <?php echo $generatedDiv; ?>
+
+</body>
+
+</html>
